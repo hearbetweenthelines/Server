5e7d219676824fcb265cf3b03cc01e51f571f52b
Rohan Subramaniam <rcsubra2@illinois.edu>
2015-10-26 19:49:23 -0500

    created get/post requests for text input

--- hearbetweenthelines/Connect.py
+++ hearbetweenthelines/Connect.py
@@ -14,3 +14,4 @@ conn = psycopg2.connect(
 
 )
 
+
--- hearbetweenthelines/views.py
+++ hearbetweenthelines/views.py
@@ -1,6 +1,11 @@
-from flask import render_template
+from flask import render_template, request, redirect
 from hearbetweenthelines import app
 
-@app.route('/')
+@app.route('/', methods =['GET','POST'])
 def home():
-    return render_template('HomePage.html')
+	if request.method == "POST":
+		mssg = request.form['message']
+		f1 = request.form['musicfile']
+		#f2 = request.form['encryptedfile']
+		print mssg, f1 #f2 
+	return render_template('HomePage.html')
