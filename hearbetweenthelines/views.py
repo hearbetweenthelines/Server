20c59fb111d8b9fc24d8493ec968a23719f8248e
ehennenfent <ehennenfent@imsa.edu>
2015-12-07 21:37:27 -0600

    Moved unused code to legacy folder

--- hearbetweenthelines/views.py
+++ hearbetweenthelines/views.py
@@ -1,7 +1,6 @@
 from flask import render_template, request, redirect, url_for
 from werkzeug import secure_filename
 from hearbetweenthelines import app, ALLOWED_EXTENSIONS
-from encrypt import extract, hide
 import os
 
 def allowed_file(filename):
@@ -27,7 +26,6 @@ def home():
                     os.system("./wavstego/wavstego -e 1234 -m Message.txt -a "+filename+" -o MixedTape.wav")
                 else:
                     os.system("./mp3stego/mp3stego -b 320 -e Message.txt -p 1234 "+filename+" MixedTape.mp3")
-                #hide("Message.txt", filename, '1', "MixedTape.mp3")
                 msg1 = "Success!"
             else:
                 msg1 = "Error: Please enter a proper music file"
@@ -36,10 +34,6 @@ def home():
             if file and allowed_file(file.filename):
                 filename = secure_filename(file.filename)
                 file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-                #text = extract(filename, '1')
-                #messageFile = open("DecodedMessage.txt","w+")
-                #messageFile.write(text)
-                #messageFile.close()
                 if filename.rsplit('.', 1)[1] == "wav":
                     os.system("./wavstego/wavstego -d 1234 -a MixedTape.wav")
                 else:
