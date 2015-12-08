2efb5a10125005689997ab60902a17f47357839a
Rohan Subramaniam <rcsubra2@illinois.edu>
2015-12-07 19:16:39 -0600

    Final product almost complete

--- hearbetweenthelines/__init__.py
+++ hearbetweenthelines/__init__.py
@@ -7,7 +7,7 @@ import getpass
 user = getpass.getuser()
 #UPLOAD_FOLDER = "/tmp"
 UPLOAD_FOLDER = '/Users/rohansubramaniam/Documents/FreshmanYear/CS196/hearbetweenthelines'
-ALLOWED_EXTENSIONS = set(['mp3'])
+ALLOWED_EXTENSIONS = set(['mp3','wav'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
--- hearbetweenthelines/views.py
+++ hearbetweenthelines/views.py
@@ -16,27 +16,35 @@ def home():
     if request.method == "POST":
         text = request.form['message']
         if text != "":
-			messageFile = open("Message.txt","w+")
-			messageFile.write(text)
-			messageFile.close()
-			file = request.files['musicfile']
-			if file and allowed_file(file.filename):
-				filename = secure_filename(file.filename)
-				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-				hide("Message.txt", filename, '1', "MixedTape.mp3")
-				msg1 = "Success!"
-			else:
-				msg1 = "Error: Please enter a proper music file"
+            messageFile = open("Message.txt","w+")
+            messageFile.write(text)
+            messageFile.close()
+            file = request.files['musicfile']
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                if filename.rsplit('.', 1)[1] == "wav":
+                    os.system("./wavstego/wavstego -e 1234 -m Message.txt -a "+filename+" -o MixedTape.wav")
+                else:
+                    os.system("./mp3stego/mp3stego -b 320 -e Message.txt -p 1234 "+filename+" MixedTape.mp3")
+                #hide("Message.txt", filename, '1', "MixedTape.mp3")
+                msg1 = "Success!"
+            else:
+                msg1 = "Error: Please enter a proper music file"
         else:
-			file = request.files['encryptedfile']
-			if file and allowed_file(file.filename):
-				filename = secure_filename(file.filename)
-				file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-				text = extract(filename, '1')
-				messageFile = open("DecodedMessage.txt","w+")
-				messageFile.write(text)
-				messageFile.close()
-				msg2 = "Success!"
-			else:
-				msg2 = "Error: Please enter a proper music file"
+            file = request.files['encryptedfile']
+            if file and allowed_file(file.filename):
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                #text = extract(filename, '1')
+                #messageFile = open("DecodedMessage.txt","w+")
+                #messageFile.write(text)
+                #messageFile.close()
+                if filename.rsplit('.', 1)[1] == "wav":
+                    os.system("./wavstego/wavstego -d 1234 -a MixedTape.wav")
+                else:
+                    os.system("./mp3stego/mp3stego -p 1234 MixedTape.mp3")
+                msg2 = "Success!"
+            else:
+                msg2 = "Error: Please enter a proper music file"
     return render_template('index.html',message1 = msg1, message2 = msg2)
